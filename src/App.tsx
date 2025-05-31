0c17711ac332c5cda6b84912546c9d99518c21f5
ohrampage <ohrampage@proton.me>
2025-05-31 16:45:55 +0100

    chore: setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen grid place-items-center'>
+      <h2 className='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0'>Hello, fivem!</h2>
+    </div>
+  )
+}
+
+export default App
